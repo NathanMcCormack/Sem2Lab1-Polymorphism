571c28e7c8dd7726fd8fbca05fa6e99919a87656
NathanMcCormack <G00414780@atu.ie>
2024-01-31 16:26:14 +0000

    Added two classes Music & TV

---
+++ src/main/java/Productv5/TV.java
@@ -0,0 +1,34 @@
+package Productv5;
+
+public class TV extends Product{
+    private String ScreenSize;
+    private String manufacturer;
+
+    public TV (){
+        super();
+        ScreenSize = "";
+        manufacturer = "";
+        count++;
+    }
+
+    public String getScreenSize() {
+        return ScreenSize;
+    }
+
+    public void setScreenSize(String screenSize) {
+        ScreenSize = screenSize;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "TV{" + "ScreenSize='" + ScreenSize + '\'' + ", manufacturer='" + manufacturer + '\'' + '}';
+    }
+}
